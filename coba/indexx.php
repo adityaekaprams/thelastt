fc751edff4862fcb451c823cf81a90955a9b521c
adityaekaprams <63759425+adityaekaprams@users.noreply.github.com>
2020-04-25 12:43:16 +0700

    Add files via upload

---
+++ coba/indexx.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>liberty.com</title>
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+  <!-- navbar -->
+  <nav class="navbar navbar-default navbar-static-top">
+    <div class="container" id="navbar">
+    <a href="http://localhost/thelast/loginn/indexx.php" class="navbar-brand navbar-link"><img src="assets/img/lo.png" height="200px" ></a>
+      <div class="navbar-header">
+        <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+      </div>
+      <div class="collapse navbar-collapse" id="navcol-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li role="presentation"><a href="#home">HOME</a></li>
+          <li role="presentation"><a href="#news">NEWS</a></li>
+          <li role="presentation"><a href="#gallery">GALLERY</a></li>
+          <li role="presentation"><a href="#about">PROFILE</a></li>
+          <li role="presentation"><a href="http://localhost/thelast/kamar/view.php">VIEW</a></li>
+          <li role="presentation"><a href="http://localhost/thelast/loginn/indexx.php">LOGIN</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+       
+  <!-- navbar -->
+
+  <!-- jumbotron -->
+  <div id="background">
+    <div class="jumbotron">
+              <h1>Hotel.</h1>
+      <p>Kami adalah hotel terbaik termewah dan paling tercanggih di dunia ini, kami adalah hotel nomer satu di dunia</p>
+      
+  
+    </div>
+  </div>
+  <!-- jumbotron -->
+
+  <!-- container atas -->
+  <div id="home">
+    <div class="isi">
+      <div class="container atas">
+        <h1>Tentang apa itu hotel kami</h1>
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+            <i class="glyphicon glyphicon-map-marker"></i>
+            <h3>LOKASI STRATEGIS</h3>
+            <p class="text-justify"><strong></strong>lokasi kami terletak di rungkut yang dimana daerah tersebut sangatlah strategis karena berdekatan dengan tempat tempat, kita dekat dengan terminal, pelabuhan, jalan tol, serta bandara</p>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+            <i class="glyphicon glyphicon-tree-conifer"></i>
+            <h3>RINDANG DAN MODERN</h3>
+            <p class="text-justify"><strong></strong>meskipun kami mengusung tema modern, kami juga tidak lupa memberikan fasilitas taman yang rindang dan nyaman agar suasana yang dihasilkan menjadi lebih nikmat dan seimbang</p>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+            <i class="glyphicon glyphicon-camera"></i>
+            <h3>INSTAGRAMABLE</h3>
+            <p class="text-justify"><strong></strong>fasilitas yang modern kami merasa kurang cukup untuk pengunjung jadi kita berinisiati memberikan ruang yang menarik atau instagramable agar pengunjung bisa berselfie ria</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- container atas -->
+
+  <!-- container bawah -->
+  <div class="container bawah">
+    <h2>Preview Hotel</h2>
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"><img class="img" src="assets/img/bunder/lokasi.jpg" width="250px">
+        <h3>STRATEGIS</h3>
+        <p class="text-center"><strong></strong>terletak di jl.yufidare 03 kota surabaya jawa timur.</p>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"><img class="img" src="assets/img/bunder/alam.jpg" width="250px">
+        <h3>RINDANG MODERN</h3>
+        <p class="text-center"><strong></strong>tempat ini bernama the legend of modern and nature.</p>
+      </div><div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"><img class="img" src="assets/img/bunder/instagram.jpg" width="250px">
+        <h3>MENARIK</h3>
+        <p class="text-center"><strong></strong>ruang instagramable yang bernama fegebedu room.</p>
+      </div>
+    </div>
+  </div>
+  <!-- container bawah -->
+
+  <!-- container news -->
+  <div id="news">
+    <div class="container">
+        <center>
+        <h2>artis yang pernah menginap</h2>
+</center>
+      <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <div class="thumbnail"><img src="assets/img/fotoartis/2atta.jpg">
+          <div class="caption">
+            <h3>Atta Halilintar</h3>
+            <p class="text-justify">atta datang dan menginap kesini karena dia ada acara penting yaitu dia diundang di acara the youtube fanfest indonesia yang ada disurabaya</p>
+          </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <div class="thumbnail"><img src="assets/img/fotoartis/angga.jpeg">
+          <div class="caption">
+            <h3>Angga Yunanda</h3>
+            <p class="text-justify">angga aldiyunanda pernah menginap disini karena dia ada salah satu acara yang ada di mall besar surabaya yaitu acara gala premiere filmnya dia.</p>
+          </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <div class="thumbnail"><img src="assets/img/fotoartis/4zara.png">
+          <div class="caption">
+            <h3>Adhisty zara jkt48</h3>
+            <p class="text-justify">adhisty zara ex jkt48 pernah meninap disini karena dia juga sama dengan angga yunanda yaitu gala premiere di satu film yang sama sehingga mereka menginap bersamaan</p>
+          </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <div class="thumbnail"><img src="assets/img/fotoartis/syahrini.jpg">
+          <div class="caption">
+            <h3>Syahrini</h3>
+            <p class="text-justify">syahrini pernah menginap disini saat dia terdapat undangan acara dari brand kosmetik yang di brand ambassador i oleh dia sendiri di daerah surabaya</p>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- container news -->
+
+  <!-- gallery -->
+  <div id="gallery">
+    <div class="container">
+      <h1>Gallery</h1>
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" id="satu"><img class="img-responsive" src="assets/img/hotel2.jpg" width="3000px"></div>
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3" id="satu"><img class="img-responsive" src="assets/img/hotel.jpg" width="300px"></div>
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3" id="satu"><img class="img-responsive" src="assets/img/hotel1.jpg" width="280px"></div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" id="dua"> <img class="img-responsive" src="assets/img/hotel33.jpg" width="469px"></div>
+      </div>
+    </div>
+  </div>
+  <!-- gallery -->
+
+  <!-- about -->
+  <div id="about">
+    <div class="container footer">
+      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+        <h1> <img src="assets/img/logooo0.png" width="120px"></h1>
+        <h2>Profile Maker</h2>
+        <p>haloo, perkenalkan nama saya aditya eka prameswara no. 01 dari kelas XRPL7. Saya bersekolah di SMK Telkom Malang dengan tekat, harapan serta cita-cita yang sangat kuat untuk mencapai keinginan yang saya inginkan, Sekian.</p>
+      </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          <h2>Social media saya nih</h2>
+          <div id="icon"><a href="https://www.instagram.com/prameswara.ae/"><i class="fa fa-instagram"></i></a>
+          <a href="https://www.facebook.com/"><i class="fa fa-facebook-official"></i></a>
+          <a href="https://www.twitter.com/"><i class="fa fa-twitter-square"></i></a>
+          <a href="https://www.youtube.com/channel/UCcM8wi1AoNMIK-4mE-bZC0Q?view_as=subscriber"><i class="fa fa-youtube-play"></i></a></div>
+        </div>
+    </div>
+  </div>
+  <!-- about -->
+
+  <!-- kaki -->
+  <div id="kaki">
+    <div class="container">
+      <h5 class="text-center">liberty. © 2018</h5>
+    </div>
+  </div>
+  <!-- kaki -->
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
